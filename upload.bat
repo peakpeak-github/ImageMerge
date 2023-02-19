7d4694ee1dcf20eff9ccf401aae4eb536b6e2aab
Peak <peakpeak-github@users.noreply.github.com>
2023-02-19 15:53:57 +0100

    Update upload.bat

--- upload.bat
+++ upload.bat
@@ -1,1 +1,1 @@
-Esptool.exe --baud 115200 --after no_reset write_flash --flash_size detect --flash_mode qio 0x0 everything.bin
+esptool.exe --baud 115200 --after no_reset write_flash --flash_size detect --flash_mode qio 0x0 everything.bin
